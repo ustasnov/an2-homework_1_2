3634ba150335cd66bb8fdb8b95f5a20565ccb71e
ustasnov <ustasnov@gmail.com>
2023-03-21 23:47:01 +0700

    Changes for task4_3

---
+++ app/src/main/java/ru/netology/nmedia/service/FCMService.kt
@@ -0,0 +1,126 @@
+package ru.netology.nmedia.service
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.google.gson.Gson
+import ru.netology.nmedia.R
+import ru.netology.nmedia.actions.Action
+import ru.netology.nmedia.actions.AddNewPost
+import ru.netology.nmedia.actions.Like
+import kotlin.math.min
+import kotlin.random.Random
+
+
+class FCMService : FirebaseMessagingService() {
+    private val action = "action"
+    private val content = "content"
+    private val channelId = "remote"
+    private val gson = Gson()
+
+    override fun onCreate() {
+        super.onCreate()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_remote_name)
+            val descriptionText = getString(R.string.channel_remote_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, name, importance).apply {
+                description = descriptionText
+            }
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        try {
+            message.data[action]?.let {
+                when (Action.valueOf(it)) {
+                    Action.LIKE -> handleLike(
+                        gson.fromJson(
+                            message.data[content],
+                            Like::class.java
+                        )
+                    )
+                    Action.ADDNEWPOST -> handleAddNewPost(
+                        gson.fromJson(
+                            message.data[content],
+                            AddNewPost::class.java
+                        )
+                    )
+                }
+            }
+        } catch (e: java.lang.IllegalArgumentException) {
+            handleActionNotFound()
+        }
+    }
+
+    override fun onNewToken(token: String) {
+        println(token)
+    }
+
+    private fun handleLike(content: Like) {
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle(
+                getString(
+                    R.string.notification_user_liked,
+                    content.userName,
+                    content.postAuthor,
+                )
+            )
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        NotificationManagerCompat.from(this)
+            .notify(Random.nextInt(100_000), notification)
+    }
+
+    private fun handleAddNewPost(content: AddNewPost) {
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle(
+                getString(
+                    R.string.notification_add_new_post,
+                    content.userName,
+                ))
+            .setContentText(content.content)
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText(content.content
+                        .slice(0..min(content.content.length - 1, 300)).plus("..."))
+            )
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        NotificationManagerCompat.from(this)
+            .notify(Random.nextInt(100_000), notification)
+    }
+
+    private fun handleActionNotFound() {
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle(
+                getString(
+                    R.string.new_notification,
+                )
+            )
+            .setContentText(
+                getString(
+                    R.string.notification_update_app,
+                )
+            )
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        NotificationManagerCompat.from(this)
+            .notify(Random.nextInt(100_000), notification)
+    }
+
+}
+
