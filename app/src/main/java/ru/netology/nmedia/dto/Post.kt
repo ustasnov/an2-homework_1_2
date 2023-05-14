32f3e54e7a92728ad703208457c5f4d065d84ba4
ustasnov <ustasnov@gmail.com>
2023-05-15 01:48:05 +0700

    Changes for task1_6

--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -3,11 +3,23 @@ package ru.netology.nmedia.dto
 data class Post(
     val id: Long,
     val author: String,
+    val authorAvatar: String = "",
     val content: String,
     val published: String,
     val likedByMe: Boolean,
     val likes: Double,
     val shared: Double,
     val views: Double,
-    val video: String?
+    val video: String?,
+    var attachment: Attachment? = null,
 )
+
+data class Attachment(
+    val url: String,
+    val description: String,
+    val type: AttachmentType,
+)
+
+enum class AttachmentType {
+    IMAGE
+}
