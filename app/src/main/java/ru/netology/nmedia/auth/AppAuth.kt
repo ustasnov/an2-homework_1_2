b7a5b00ee04801d8dda0b0612fa05301d3470f10
ustasnov <ustasnov@gmail.com>
2023-07-06 00:46:49 +0700

    Changes for task 1_14

--- app/src/main/java/ru/netology/nmedia/auth/AppAuth.kt
+++ app/src/main/java/ru/netology/nmedia/auth/AppAuth.kt
@@ -3,8 +3,15 @@ package ru.netology.nmedia.auth
 import android.content.Context
 import android.content.SharedPreferences
 import androidx.core.content.edit
+import com.google.firebase.messaging.FirebaseMessaging
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import ru.netology.nmedia.api.Api
+import ru.netology.nmedia.dto.PushToken
 import ru.netology.nmedia.dto.Token
 
 class AppAuth private constructor(
@@ -13,6 +20,7 @@ class AppAuth private constructor(
     companion object {
         private const val TOKEN_KEY = "TOKEN_KEY"
         private const val ID_KEY = "ID_KEY"
+
         @Volatile
         private var INSTANCE: AppAuth? = null
 
@@ -25,7 +33,9 @@ class AppAuth private constructor(
             "You must call initApp before"
         }
     }
-    private val prefs: SharedPreferences = context.getSharedPreferences("auth", Context.MODE_PRIVATE)
+
+    private val prefs: SharedPreferences =
+        context.getSharedPreferences("auth", Context.MODE_PRIVATE)
     private val _data = MutableStateFlow<Token?>(null)
     val data = _data.asStateFlow()
 
@@ -34,10 +44,15 @@ class AppAuth private constructor(
         val id = prefs.getLong(ID_KEY, 0L)
 
         if (token == null || id == 0L) {
-            prefs.edit { clear() }
+            _data.value = null
+            prefs.edit {
+                clear()
+                apply()
+            }
         } else {
             _data.value = Token(id, token)
         }
+        sendPushToken()
     }
 
     @Synchronized
@@ -47,11 +62,26 @@ class AppAuth private constructor(
             putString(TOKEN_KEY, token.token)
             putLong(ID_KEY, token.id)
         }
+        sendPushToken()
     }
 
     @Synchronized
     fun clearAuth() {
         _data.value = null
         prefs.edit { clear() }
+        sendPushToken()
+    }
+
+    fun sendPushToken(token: String? = null) {
+        CoroutineScope(Dispatchers.Default).launch {
+            try {
+                val request = PushToken(token ?: FirebaseMessaging.getInstance().token.await())
+                Api.service.sendPushToken(request)
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+        }
     }
 }
